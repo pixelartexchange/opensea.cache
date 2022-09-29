02dc48ee625b448323f4373d5ea0d74b72310eee
Gerald Bauer <gerald.bauer@gmail.com>
2022-09-29 20:54:30 +0200

    up

--- sandbox/download.rb
+++ sandbox/download.rb
@@ -64,11 +64,11 @@ slugs = %w[
   frontphunks
   genius-punks
   ghozalipunk
-]
-
-slugs = %w[
   goodbye-punks
 ]
+
+# slugs = %w[
+# ]
 
 
 delay_in_s = 2
@@ -98,8 +98,51 @@ slugs.each do |slug|
   data_stats  = data['stats']
   data.delete( 'stats')
 
-  data_payment = data['payment_tokens']
+  data_payments = data['payment_tokens']
   data.delete( 'payment_tokens' )
+
+  ## clean payment - remove keys:
+  ##   - eth_price
+  ##   - usd_price
+  ##  - will change daily
+  data_payments = data_payments.map do |payment|
+                                        payment.delete( 'eth_price' )
+                                        payment.delete( 'usd_price' )
+                                        payment
+                                    end
+
+  ## clean collection  - remove deprecated (duplicate keys):
+  ##  - dev_buyer_fee_basis_points
+  ##  - dev_seller_fee_basis_points
+  ##  - opensea_buyer_fee_basis_points
+  ##  - opensea_seller_fee_basis_points
+  ##  - payout_address
+  data.delete( 'dev_buyer_fee_basis_points' )
+  data.delete( 'dev_seller_fee_basis_points' )
+  data.delete( 'opensea_buyer_fee_basis_points' )
+  data.delete( 'opensea_seller_fee_basis_points' )
+  data.delete( 'payout_address' )
+
+
+  ## clean collection - remove deprecated (duplicate keys):
+  ##   - dev_buyer_fee_basis_points
+  ##   - dev_seller_fee_basis_points
+  ##   - opensea_buyer_fee_basis_points
+  ##   - opensea_seller_fee_basis_points
+  ##   - buyer_fee_basis_points
+  ##   - seller_fee_basis_points
+  ##   - payout_address
+  data_contracts = data_contracts.map do |contract|
+                                    contract.delete( 'dev_buyer_fee_basis_points' )
+                                    contract.delete( 'dev_seller_fee_basis_points' )
+                                    contract.delete( 'opensea_buyer_fee_basis_points' )
+                                    contract.delete( 'opensea_seller_fee_basis_points' )
+                                    contract.delete( 'buyer_fee_basis_points' )
+                                    contract.delete( 'seller_fee_basis_points' )
+                                    contract.delete( 'payout_address' )
+                                    contract
+                                      end
+
 
 
   cache_dir = if data_contracts.size > 0
@@ -118,14 +161,20 @@ slugs.each do |slug|
     write_json( path, data_contracts )
   end
 
-  path = "#{cache_dir}/#{slug}/traits.json"
-  write_json( path, data_traits )
+  ## FileUtils.remove_file( "#{cache_dir}/#{slug}/traits.json" )
+
+  if data_traits.size > 0
+    path = "#{cache_dir}/#{slug}/traits.json"
+    write_json( path, data_traits )
+  end
+
 
   path = "#{cache_dir}/#{slug}/stats.json"
   write_json( path, data_stats )
 
-  path = "#{cache_dir}/#{slug}/payment.json"
-  write_json( path, data_payment )
+
+  path = "#{cache_dir}/#{slug}/payments.json"
+  write_json( path, data_payments )
 
 
   puts "  sleeping #{delay_in_s}s..."
